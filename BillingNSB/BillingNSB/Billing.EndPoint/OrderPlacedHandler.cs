b6326721347f68fbe22d3aff8fcf7e5593076f92
Probe-House <bheer31@gmail.com>
2018-10-28 10:18:28 -0500

    first commit

---
+++ BillingNSB/BillingNSB/Billing.EndPoint/OrderPlacedHandler.cs
@@ -0,0 +1,23 @@
+﻿using Billing.Contracts.Events;
+using NServiceBus;
+using NServiceBus.Logging;
+using Sales.Contracts.Events;
+using System.Threading.Tasks;
+
+namespace Billing.EndPoint
+{
+    public class OrderPlacedHandler : IHandleMessages<OrderPlacedEvent>
+    {
+        static ILog log = LogManager.GetLogger<OrderPlacedHandler>();
+
+        public Task Handle(OrderPlacedEvent message, IMessageHandlerContext context)
+        {
+            log.Info($"Received OrderPlaced, OrderId = {message.OrderId} - Charging credit card...");
+            var orderBilled = new OrderBilledEvent
+            {
+                OrderId = message.OrderId
+            };
+            return context.Publish(orderBilled);
+        }
+    }
+}
